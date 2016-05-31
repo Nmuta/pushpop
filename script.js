c5f7ea83714cd0d9213c22495f5788b4858a8f67
Tom <tomhendersonfl@gmail.com>
2016-05-31 08:43:50 -0600

    completed

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+var counter = 0;
+var collection = [];
 $(document).ready(function (){
   console.log("welcome ! ");
-  var collection = [];
 })
-
 
 $("#trigger").on("click", function(){
   console.log("button has been clicked !");
-})
+//  var add = document.getElementById("add_stuff").value;
+  if document.getElementById("add_stuff").value != " ") {
+  collection.push(add);
+  var feedback = "<ul>";
+  for (i=0; i<collection.length; i++) {
+    feedback += ("<li>" + collection[i] + "</li>");
+  }
+  feedback += "</ul>";
+  $(".feedback").html(feedback);
+  document.getElementById("add_stuff").value=" ";
+});
